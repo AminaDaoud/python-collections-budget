8fe18219ef74cd1531424235c18e7260a7aef082
Amina Daoud <amina.daoud.tunisie@gmail.com>
2021-09-21 00:58:48 +0200

    category by number of purchases

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -20,7 +20,9 @@ for expense in expenses.list:
 spending_counter = collections.Counter(spending_categories)
 top5 = spending_counter.most_common(5)
 
+
 categories, count = zip(*top5)
+
 
 fig, ax = plt.subplots()
 ax.bar(categories, count)
